7fbfb9239b25388cd6523293580250518ca5ea7a
Michael Rubanov <mrubanov@gmail.com>
2016-02-09 22:00:57 +0200

    .

--- index.js
+++ index.js
@@ -3,13 +3,36 @@
 
 //Lets require/import the HTTP module
 var http = require('http');
+var fs = require('fs');
+const PRFX = "webhook-request-debug";
+const SP = "-";
+
+var writeToLog = function (content) {
+  var logFile = "/tmp/";
+  var hs = Math.round((new Date()).getTime() / (60 * 1000 * 60));
+  logFile = logFile + PRFX + hs + ".log";
+  fs.appendFile(logFile, content + "\n", function(err) {
+    if(err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
+};
 
 //Lets define a port we want to listen to
 const PORT=5009;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+  var responseString = '';
+  request.on('data', function (chunk) {
+    responseString += chunk;
+  });
+  request.on('end', function () {
+    writeToLog(JSON.stringify(responseString));
+  });
+
+  response.end('It Works!! Path Hit: ' + request.url);
 }
 
 //Create a server
